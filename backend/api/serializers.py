22f70f360038d9dac759d8fe4072782775dd1ec9
Nalukoola <ndaulachris@gmail.com>
2024-07-18 13:38:54 +0300

    Changes

---
+++ backend/api/serializers.py
@@ -0,0 +1,21 @@
+from dataclasses import fields
+from pyexpat import model
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+class UserSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
+
+class NoteSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = { "author": {"read_only": True} }
